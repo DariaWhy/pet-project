ab265fae17ff2adcf48caca96837413449ebe604
Daria <dashaivd@gmail.com>
2025-03-17 17:11:55 +0300

    add js file with validation

---
+++ contacts.js
@@ -0,0 +1,69 @@
+let submitButton = document.getElementById('submit-button');
+
+const sendData = function () {
+    let emailField = document.getElementById('email-contact-form');
+    let phoneField = document.getElementById('phone-number-form');
+    let messageField = document.getElementById('message-contact-form');
+
+    if (!emailField.value || !phoneField.value || !messageField.value) {
+        showWarning(emailField, "Please enter your email.");
+        showWarning(phoneField, "Please enter your phone number.");
+        showWarning(messageField, "Please enter your message.");
+        return; 
+    }
+
+    let messageData = {
+        userEmail: emailField.value,
+        userNumber: phoneField.value,
+        textToSend: messageField.value,
+    };
+
+    localStorage.setItem('formData', JSON.stringify(messageData));
+
+    document.cookie = `userEmail=${messageData.userEmail}; max-age=604800; path=/`;
+    document.cookie = `userNumber=${messageData.userNumber}; max-age=604800; path=/`;
+    document.cookie = `textToSend=${messageData.textToSend}; max-age=604800; path=/`;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(messageData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Успешно:', data);
+        emailField.value = '';
+        phoneField.value = '';
+        messageField.value = '';
+
+        document.querySelectorAll('.warning').forEach(warning => warning.remove());
+    })
+    .catch(error => console.error('Ошибка:', error));
+};
+
+
+
+function showWarning(inputElement, message) {
+    const parent = inputElement.parentNode;
+    let warning = parent.querySelector('.warning'); 
+    if (!inputElement.value) {
+        if (!warning) {
+            warning = document.createElement('p');
+            warning.className = 'warning'; 
+            warning.textContent = message;
+            warning.style.color = 'red';
+            warning.style.fontSize = '0.8rem';
+            warning.style.marginTop = '5px';
+            parent.appendChild(warning);
+        }
+    } else {
+        if (warning) {
+            warning.remove(); 
+        }
+    }
+}
+
+submitButton.addEventListener('click', sendData);
+
