0836c1236756164797e2f9b2582f712aef755c81
Daria <dashaivd@gmail.com>
2025-03-21 13:22:22 +0300

    change styles in dining add buttons for rooms on main page

--- script.js
+++ script.js
@@ -32,6 +32,23 @@ window.onload = function () {
         });
     });
 
+
+    const buttons = document.querySelectorAll('.rooms-buttons');
+    const rooms = document.querySelectorAll('.suit-class');
+
+    rooms.forEach((room, index) => {
+        if (index !== 0) room.style.display = 'none';
+      });
+      
+      buttons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+          rooms.forEach(room => (room.style.display = 'none'));
+          rooms[index].style.display = 'flex';
+          buttons.forEach(btn => btn.classList.remove('active-button'));
+          button.classList.add('active-button');
+        });
+    });
+
 };
 
    
